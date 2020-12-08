4987d94597dcc153ffc7ade7a7dab57d06869801
Tobey Carman <tobeycarman@gmail.com>
2020-12-08 14:35:56 -0900

    Add some R code that generates the cimin and cimax columns.

--- posterior_table.sh
+++ posterior_table.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# T. Carman
+# T. Carman, Dec 2020
+
+# NOTE: Not designed to be run as a script! Use by copying relevant 
+# bits of code to the appropriate interperter...
 
 # Collate a posteriors table or priors table.
 # This code works the same for priors if you substitute 
 # "prior.distns.Rdata" for "post.disnts.MA.Rdata"
-#
 #
 # The posterior distribution data exists in .Rdata files in 
 # the run folders like this:
@@ -56,6 +58,7 @@ for f in $(find tbc-scratch/ -name "post.distns.MA.Rdata.csv"); do
 done
 
 
+## ---------------------------------------------------------------------------
 # The following code is python
 # This changes the names of the traits/parameters for easier reading...
 # matching the parameter names that Eugenie used in her priors spreadsheet
@@ -107,17 +110,54 @@ with open("posteriors_table.csv", 'w') as f:
   f.writelines(data)
 
 
+##------------------------------------------------------------------------------
+# Now finally, some R code that will add in the columns for 
+# confidence interval bounds
 
+# This function was copied from the prior_stats_image.R script
+# (in this repo) which was copied from the PEcAn codebase
+pr.dens <- function(distn, parama, paramb, n = 1000, alpha = 0.001) {
+  alpha <- ifelse(alpha < 0.5, alpha, 1-alpha)
+  n <- ifelse(alpha == 0.5, 1, n)
+  range.x <- do.call(paste('q', distn, sep = ""), list(c(alpha, 1-alpha), parama, paramb))
+  seq.x   <- seq(from = range.x[1], to = range.x[2], length.out = n)
+  dens.df <- data.frame(x = seq.x,
+                        y = do.call(paste('d', distn, sep=""),
+                                    list(seq.x, parama, paramb)))
+  return(dens.df)
+}
 
-# Some R, not particularly useful...
-# for(trait in seq(1,ncol(prior.distns))){
-#     for(prop in seq(1,nrow(prior.distns))) {
-#         #print(c(rownames(prior.distns)[prop],"--->", prop, prior.distns[]))
-#         if (is.numeric(prior.distns[[prop,trait]]) && is.numeric(post.distns[[prop,trait]])) {
-#             delta = prior.distns[[prop,trait]] - post.distns[[prop,trait]]
-#         } else {
-#             delta = "-"
-#         }
-#         print(c(delta, rownames(prior.distns)[prop], prior.distns[[prop,trait]], post.distns[[prop,trait]]), right = true)
-#     }
-# }
+# A function to get the confidence interval bounds, given the 
+# distribution name and parameters
+get.ci <- function(distn, parama, paramb, n = 1000, alpha = .001) {
+  pd = pr.dens(distn, parama, paramb)
+  cdf <- data.frame(x=pd$x, y=do.call(paste("p",sep="",distn), list(pd$x,parama,paramb)))
+  cimin <- cdf$x[cdf$y<=0.025][1]
+  cimax <- cdf$x[cdf$y>=0.975][1]
+  return(list(cimin, cimax))
+}
+
+# Read in our existing table that we created above - works the same for the 
+# priors table
+df <- read.csv("tbc-scratch/posteriors_table.csv")
+
+# make some empty lists
+cimin <- c()
+cimax <- c()
+# Fill the lists using functions defined above
+for(i in 1:nrow(df)){
+  a <- get.ci(df$distname[i], df$parama[i], df$paramb[i])
+  cimin <- append(cimin, a[1])
+  cimax <- append(cimax, a[2])
+}
+
+# Add new columns to the dataframe
+df$cimin <- cimin
+df$cimax <- cimax
+
+df$cimin <- as.numeric(cimin)
+df$cimax <- as.numeric(cimax)
+
+# Write back to csv
+write.csv(df, file="tbc-scratch/posteriors_table.csv", dec=".", row.names=FALSE)
+
